9531faed9634ea95cf4587ecbf9097da5e1baa74
yvonnesjy <stonfish@hotmail.com>
2016-11-12 15:30:57 -0500

    Added form validation. Updated UI

--- src/pages/seller-confirmation/seller-confirmation.ts
+++ src/pages/seller-confirmation/seller-confirmation.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Utils } from '../utils/utils';
+// import { Utils } from '../utils/utils';
 
 
 @Component({
--- src/pages/seller-suggested-books/seller-suggested-books.ts
+++ src/pages/seller-suggested-books/seller-suggested-books.ts
@@ -1,39 +1,52 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { SellerSetPrice } from '../seller-set-price/seller-set-price';
+import { BookService } from '../../providers/book-service';
 
 
 @Component({
-  templateUrl: 'seller-suggested-books.html'
+  templateUrl: 'seller-suggested-books.html',
+  providers: [BookService]
 })
 export class SellerSuggestedBooks {
-  suggestedBooks: Array<{title: string, author: string}>;
+  suggestedBooks: Array<{title: string, author: string, isbn: string}>;
+  course: string;
+  professor: string;
+  semester: string;
+  public list: any;
   form = {
     title: '',
     author: '',
     edition: '',
     isbn: ''};
 
-  constructor(private navCtrl: NavController, private navParams: NavParams) {
-    this.suggestedBooks = [
-      {title: 'Mobile Services Textbook',
-      author: 'John Smith, Steve Jones'},
-      {title: 'Guide to the BMC',
-      author: 'John Jacboson'},
-      {title: 'Information in the New Age',
-      author: 'John Jones, Joseph Park'},
-      {title: 'The Study of Mobile Apps',
-      author: 'John Jones, Robert Paulson'}
-    ];
+  constructor(private navCtrl: NavController, private navParams: NavParams, public bookService: BookService) {
+    this.list = null;
+    this.suggestedBooks = [];
+
+    let classForm = this.navParams.get('classForm');
+    this.course = classForm.course;
+    this.professor = classForm.instructor;
+    this.semester = classForm.semester;
+    this.loadList();
+  }
+
+  loadList(){
+    this.bookService.load(this.course, this.professor)
+    .then(data => {
+      this.list = data;
+      for(let l of this.list) {
+        this.suggestedBooks.push({title: l.title, author: l.author, isbn: l.isbn});
+      }
+    });
   }
 
   openPage(suggestedBook) {
-    let classForm = this.navParams.get('classForm');
     this.navCtrl.push(SellerSetPrice, {
       book: suggestedBook,
-      course: classForm.course,
-      instructor: classForm.instructor,
-      semester: classForm.semester});
+      course: this.course,
+      instructor: this.professor,
+      semester: this.semester});
   }
 
   onSubmit() {
